f9d87057e007ca3421d4ba2190d1da9990bca73b
Tim Ringrose <73766725+tjringrose01@users.noreply.github.com>
2026-05-05 18:58:11 -0400

    Add JSON parser dependency for menu definitions
    Refs #3

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,14 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
-class ExampleRecipe(ConanFile):
+
+class CursedMenuRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
         self.requires("ncurses/6.5")
+        self.requires("nlohmann_json/3.11.3")
 
     def layout(self):
         cmake_layout(self)
