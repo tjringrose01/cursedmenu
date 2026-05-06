2e32b41b3a98cf0ebbc01ce2cebcfe0290387d76
Tim Ringrose <73766725+tjringrose01@users.noreply.github.com>
2026-05-06 18:34:22 -0400

    Modernize CursedMenuLoader implementation
    Refs #11

--- source/libCursedMenu/CursedMenuLoader.cpp
+++ source/libCursedMenu/CursedMenuLoader.cpp
@@ -26,7 +26,7 @@
  *     appended array.
  *  5. The Main Menu completes it's processing and is added to the end
  *     of the vector.
- * 
+ *
  * ---------------------------------------------------------------------------
  *
  *  Copyright 2007, 2008, 2024 Timothy Ringrose
@@ -48,40 +48,160 @@
  *
  */
 
-#include <vector>
-#include <string>
-#include <fstream>
-#include <ostream>
-#include <iostream>
+#include <cctype>
 #include <curses.h>
-#include "CursedMenuLoader.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+
 #include "CursedMenu.hpp"
 #include "CursedMenuItem.hpp"
+#include "CursedMenuLoader.hpp"
 #include "debug.hpp"
 
 #define PROGRAM "CursedMenuLoader"
 
-CursedMenu CursedMenuLoader::load(const std::string configFile, const bool debugFlag)
-{
-    std::vector<CursedMenu> menus = loadConfig(configFile, debugFlag);
-    return(menus.at(menus.size() - 1));
+namespace {
+
+std::string trimLeadingSpaces(std::string value) {
+    const auto firstNonSpace = value.find_first_not_of(' ');
+
+    if (firstNonSpace == std::string::npos) {
+        return "";
+    }
+
+    return value.substr(firstNonSpace);
 }
 
-std::vector<CursedMenu> CursedMenuLoader::loadConfig(const std::string configFile, const bool debugFlag) {
+std::string toUpperAscii(const std::string& value) {
+    std::string result;
+    result.reserve(value.length());
 
-    std::cout << "CursedMenuLoader::loadConfig(" << configFile << ", " << debugFlag << ");" << std::endl;
+    for (const unsigned char character : value) {
+        result += static_cast<char>(std::toupper(character));
+    }
+
+    return result;
+}
+
+int foregroundColorFromName(const std::string& colorName) {
+    const std::string color = toUpperAscii(colorName);
+
+    if (color == "BLACK") {
+        return COLOR_BLACK;
+    }
+
+    if (color == "RED") {
+        return COLOR_RED;
+    }
+
+    if (color == "GREEN") {
+        return COLOR_GREEN;
+    }
+
+    if (color == "BLUE") {
+        return COLOR_BLUE;
+    }
+
+    if (color == "YELLOW") {
+        return COLOR_YELLOW;
+    }
+
+    if (color == "MAGENTA") {
+        return COLOR_MAGENTA;
+    }
+
+    if (color == "CYAN") {
+        return COLOR_CYAN;
+    }
+
+    return COLOR_WHITE;
+}
+
+int backgroundColorFromName(const std::string& colorName) {
+    const std::string color = toUpperAscii(colorName);
+
+    if (color == "BLACK") {
+        return COLOR_BLACK;
+    }
+
+    if (color == "RED") {
+        return COLOR_RED;
+    }
+
+    if (color == "GREEN") {
+        return COLOR_GREEN;
+    }
+
+    if (color == "BLUE") {
+        return COLOR_BLUE;
+    }
+
+    if (color == "YELLOW") {
+        return COLOR_YELLOW;
+    }
+
+    if (color == "MAGENTA") {
+        return COLOR_MAGENTA;
+    }
+
+    if (color == "CYAN") {
+        return COLOR_CYAN;
+    }
+
+    if (color == "WHITE") {
+        return COLOR_WHITE;
+    }
+
+    return COLOR_BLACK;
+}
+
+bool containsToken(
+    const std::string& buffer,
+    const std::string& token) {
+    return buffer.find(token) != std::string::npos;
+}
+
+std::string valueAfterEquals(const std::string& buffer) {
+    const auto equalsPosition = buffer.find('=');
+
+    if (equalsPosition == std::string::npos) {
+        return "";
+    }
+
+    return trimLeadingSpaces(buffer.substr(equalsPosition + 1));
+}
+
+} // namespace
+
+CursedMenu CursedMenuLoader::load(
+    const std::string& configFile,
+    const bool debugFlag) {
+    const std::vector<CursedMenu> menus = loadConfig(configFile, debugFlag);
+
+    return menus.at(menus.size() - 1);
+}
+
+std::vector<CursedMenu> CursedMenuLoader::loadConfig(
+    const std::string& configFile,
+    const bool debugFlag) {
+    std::cout
+        << "CursedMenuLoader::loadConfig("
+        << configFile
+        << ", "
+        << debugFlag
+        << ");"
+        << std::endl;
+
     std::vector<CursedMenu> menus;
-    std::vector<CursedMenu> tmp_menus;
-
-    CursedMenu* curMenu = new CursedMenu();
-
-    //Removed for compatability on older g++ compilers
-    //menus.clear();
     menus.reserve(25);
 
+    CursedMenu currentMenu;
+
     std::string buffer;
-    std::string color;
-    std::string temp_color;
     std::string name;
     std::string desc;
     std::string exec;
@@ -90,205 +210,136 @@ std::vector<CursedMenu> CursedMenuLoader::loadConfig(const std::string configFil
     bool getTitl = false;
     bool getItem = false;
 
-    std::ifstream file_in( configFile.c_str() );
+    std::ifstream fileInput(configFile.c_str());
 
-    if ( file_in.is_open() ) {
-        if (debugFlag) debug(PROGRAM, 0, "Reading config file - " + configFile);
-        while (! file_in.eof() ) {
-            getline ( file_in, buffer );
-
-            if ( buffer.length() < 6 )
-                continue;
-
-            std::string::size_type i = buffer.find_first_not_of ( " \t\n\v" );
-
-            if ( i != std::string::npos && buffer[i] == '#' )
-                continue;
-
-            /* Find MenuBegin */
-            if ( ! getMenu ) {
-                i = buffer.find("MenuBegin");
-                if ( i != std::string::npos ) {
-                    getMenu = true;
-                    continue;
-                }
-            }
-
-            i = buffer.find("MenuEnd");
-            if ( i != std::string::npos ) {
-                getMenu = false;
-                continue;
-            }
-
-            if ( ! getTitl ) {
-                i = buffer.find("MenuTitle");
-                if ( i != std::string::npos ) {
-
-                    i = buffer.find("=");
-                    curMenu->setMenuTitle(buffer.substr(i+1, buffer.length()-i-1));
-                    if (debugFlag)
-                    {
-                        debug(PROGRAM, 2, "menuTitle = " + curMenu->getMenuTitle());
-                    }
-                    continue;
-                }
-            }
-
-            /**
-             * Colors defined in curses.h
-             *
-             *  COLOR_BLACK     0
-             *  COLOR_RED       1
-             *  COLOR_GREEN     2
-             *  COLOR_YELLOW    3
-             *  COLOR_BLUE      4
-             *  COLOR_MAGENTA   5
-             *  COLOR_CYAN      6
-             *  COLOR_WHITE     7
-             */
-
-            i = buffer.find("MenuFore");
-            if ( i != std::string::npos ) {
-                //TODO read in color from config and relate to curses color
-
-                color = buffer.substr( i+9, buffer.length() - i - 1 );
-
-                if ( ! color.empty() ) {
-                    //Using ineffecient toupper() in place of std::transform
-                    // for older compiler compatability
-                    temp_color = "";
-                    for(int x=0; x < color.length(); x++) {
-                        temp_color += (char)toupper((int)color[x]);
-                    }
-                    color = temp_color;
- 
-                    if ( color == "BLACK" )
-                        curMenu->setForeColor(COLOR_BLACK);
-                    else if ( color == "RED" )
-                        curMenu->setForeColor(COLOR_RED);
-                    else if ( color == "GREEN" )
-                        curMenu->setForeColor(COLOR_GREEN);
-                    else if ( color == "BLUE" )
-                        curMenu->setForeColor(COLOR_BLUE);
-                    else if ( color == "YELLOW" )
-                        curMenu->setForeColor(COLOR_YELLOW);
-                    else if ( color == "MAGENTA" )
-                        curMenu->setForeColor(COLOR_MAGENTA);
-                    else if ( color == "CYAN" )
-                        curMenu->setForeColor(COLOR_CYAN);
-                    else if ( color == "WHITE" )
-                        curMenu->setForeColor(COLOR_WHITE);
-                    else
-                        curMenu->setForeColor(COLOR_WHITE);
-                }
-                
-            }
-
-            i = buffer.find("MenuBack");
-            if ( i != std::string::npos ) {
-                //TODO read in color from config and relate to curses color
-                color = buffer.substr( i+9, buffer.length() - i - 1 );
-
-                if ( ! color.empty() ) {
-                    //Using ineffecient toupper() in place of std::transform
-                    // for older compiler compatability
-                    temp_color = "";
-                    for(int x=0; x < color.length(); x++) {
-                        temp_color += (char)toupper((int)color[x]);
-                    }
-                    color = temp_color;
-
-                    if ( color == "BLACK" )
-                        curMenu->setBackColor(COLOR_BLACK);
-                    else if ( color == "RED" )
-                        curMenu->setBackColor(COLOR_RED);
-                    else if ( color == "GREEN" )
-                        curMenu->setBackColor(COLOR_GREEN);
-                    else if ( color == "BLUE" )
-                        curMenu->setBackColor(COLOR_BLUE);
-                    else if ( color == "YELLOW" )
-                        curMenu->setBackColor(COLOR_YELLOW);
-                    else if ( color == "MAGENTA" )
-                        curMenu->setBackColor(COLOR_MAGENTA);
-                    else if ( color == "CYAN" )
-                        curMenu->setBackColor(COLOR_CYAN);
-                    else if ( color == "WHITE" )
-                        curMenu->setBackColor(COLOR_WHITE);
-                    else
-                        curMenu->setBackColor(COLOR_BLACK);
-                }
-            }
-
-            if ( ! getItem ) {
-                i = buffer.find("ItemName");
-                if ( i != std::string::npos ) {
-                    i = buffer.find("=");
-                    name = buffer.substr( i+1, buffer.length() - i - 1);
-                    while (name.substr(0,1).compare(" ") == 0)
-                    {
-                        name = name.substr(1);
-                    }
-					getItem = true;
-                    if (debugFlag) debug(PROGRAM, 2, "found item = " + name);
-					continue;
-				}
-			} else {
-                i = buffer.find("ItemDesc");
-                if ( i != std::string::npos ) {
-                    i = buffer.find("=");
-                    desc = buffer.substr( i+1, buffer.length() - i - 1);
-                    while (desc.substr(0,1).compare(" ") == 0)
-                    {
-                        desc = desc.substr(1);
-                    }
-                    if (debugFlag) debug(PROGRAM, 2, "found desc = " + desc);
-					continue;
-				} else {
-                    i = buffer.find("ItemExec");
-                    if (i != std::string::npos) {
-                        i = buffer.find("=");
-                        exec = buffer.substr(i+1, buffer.length() - i - 1);
-                        while (exec.substr(0,1).compare(" ") == 0)
-                        {
-                            exec = exec.substr(1);
-                        }
-                       
-
-                    if (debugFlag) debug(PROGRAM, 2, "found exec = " + exec);
-                    // Parse out the exec and look for sub menu to parse
-                    i = exec.find("MenuSub ");
-                    if (i != std::string::npos)
-                    {
-                        tmp_menus = loadConfig(exec.substr(8), debugFlag);
-                        menus.insert(menus.end(), tmp_menus.begin(), tmp_menus.end());
-                        tmp_menus.clear();
-                    }
-					    continue;
-					} else {
-                        i = buffer.find("ItemEnd");
-                        if ( i != std::string::npos ) {
-                            getItem = false;
-                            /* store name, desc, and exec in item object */
-                            curMenu->addItem(CursedMenuItem(name,desc,exec));
-      
-                            name = "";
-                            desc = "";
-                            exec = "";
-
-                            continue;
-                        }
-                    }
-                }
-            }
-        }
-        menus.push_back(*curMenu);
-
-        file_in.close();
-        if (debugFlag) debug(PROGRAM, 2, "close config file");
-
-    } else {
+    if (!fileInput.is_open()) {
         std::cerr << "Unable to open file: " << configFile << std::endl;
+        return menus;
     }
 
-    return(menus);
+    if (debugFlag) {
+        debug(PROGRAM, 0, "Reading config file - " + configFile);
+    }
+
+    while (std::getline(fileInput, buffer)) {
+        if (buffer.length() < 6) {
+            continue;
+        }
+
+        const auto firstContent = buffer.find_first_not_of(" \t\n\v");
+
+        if (firstContent != std::string::npos &&
+            buffer[firstContent] == '#') {
+            continue;
+        }
+
+        if (!getMenu && containsToken(buffer, "MenuBegin")) {
+            getMenu = true;
+            continue;
+        }
+
+        if (containsToken(buffer, "MenuEnd")) {
+            getMenu = false;
+            continue;
+        }
+
+        if (!getTitl && containsToken(buffer, "MenuTitle")) {
+            currentMenu.setMenuTitle(valueAfterEquals(buffer));
+
+            if (debugFlag) {
+                debug(
+                    PROGRAM,
+                    2,
+                    "menuTitle = " + currentMenu.getMenuTitle());
+            }
+
+            continue;
+        }
+
+        if (containsToken(buffer, "MenuFore")) {
+            const std::string color = valueAfterEquals(buffer);
+
+            if (!color.empty()) {
+                currentMenu.setForeColor(foregroundColorFromName(color));
+            }
+
+            continue;
+        }
+
+        if (containsToken(buffer, "MenuBack")) {
+            const std::string color = valueAfterEquals(buffer);
+
+            if (!color.empty()) {
+                currentMenu.setBackColor(backgroundColorFromName(color));
+            }
+
+            continue;
+        }
+
+        if (!getItem) {
+            if (containsToken(buffer, "ItemName")) {
+                name = valueAfterEquals(buffer);
+                getItem = true;
+
+                if (debugFlag) {
+                    debug(PROGRAM, 2, "found item = " + name);
+                }
+
+                continue;
+            }
+        } else {
+            if (containsToken(buffer, "ItemDesc")) {
+                desc = valueAfterEquals(buffer);
+
+                if (debugFlag) {
+                    debug(PROGRAM, 2, "found desc = " + desc);
+                }
+
+                continue;
+            }
+
+            if (containsToken(buffer, "ItemExec")) {
+                exec = valueAfterEquals(buffer);
+
+                if (debugFlag) {
+                    debug(PROGRAM, 2, "found exec = " + exec);
+                }
+
+                const auto submenuPosition = exec.find("MenuSub ");
+
+                if (submenuPosition != std::string::npos) {
+                    std::vector<CursedMenu> subMenus =
+                        loadConfig(
+                            exec.substr(submenuPosition + 8),
+                            debugFlag);
+
+                    menus.insert(
+                        menus.end(),
+                        subMenus.begin(),
+                        subMenus.end());
+                }
+
+                continue;
+            }
+
+            if (containsToken(buffer, "ItemEnd")) {
+                getItem = false;
+                currentMenu.addItem(CursedMenuItem(name, desc, exec));
+
+                name.clear();
+                desc.clear();
+                exec.clear();
+
+                continue;
+            }
+        }
+    }
+
+    menus.push_back(currentMenu);
+
+    if (debugFlag) {
+        debug(PROGRAM, 2, "close config file");
+    }
+
+    return menus;
 }
